e8efca95cfc9145b38a7aa4454ed7dd86bac68cb
Nathan Ruska <nruska@gmail.com>
2011-03-30 23:52:46 -0700

    Initial commit v2- sqlite1.3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '41f83db5b5ffb90f1eb05389b8edfab5193634cfe0600605555dc9c66984d0f69433cabba84344f046a1692898a8b4274ceb851c83b0fa9035610132374495b6'
